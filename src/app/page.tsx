31fb453d91d930487a3cba7ac328c2dd101951f5
Gaven Day <day.gaven@gmail.com>
2025-09-05 01:49:39 +0000

    on mobile web browsers the app doesn't like to be clicked. The create sq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const ImposterAppComponent = dynamic(
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background text-foreground pb-28">
+    <main className="min-h-screen bg-background text-foreground pb-40">
       <ImposterAppComponent />
     </main>
   );
